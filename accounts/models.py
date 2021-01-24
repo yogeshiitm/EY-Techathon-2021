df3c7b459e3269c70136dcba82df5411509faf87
Yogesh Agarwala <69758664+yogeshiitm@users.noreply.github.com>
2021-01-24 13:08:46 +0530

    myuser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy  as _
 
 # Create your models here.
+class MyUser(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(_('email address'), unique=True) # changes email to unique and blank to false
+    # REQUIRED_FIELDS = [] # removes email from REQUIRED_FIELDS
+    REQUIRED_FIELDS = ['email', 'first_name']
+
+    def __str__(self): 
+        return "{}".format(self.email)
